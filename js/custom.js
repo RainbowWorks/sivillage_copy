54093f459242c079c98c0c8962b05e6a37d7ca93
RainbowWorks <kuj4235@kakao.com>
2025-06-11 16:08:48 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,16 @@ $(function () {
     });
 
 });
+
+$(function () {
+    $('.main_product .tab_product button').on('click', function () {
+        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
+        console.log(idx);
+
+        $('.main_product .tab_product button').removeClass('on');
+        $(this).addClass('on');
+
+        $('.main_product .tab_product_content .con').removeClass('on');
+        $('.main_product .tab_product_content .con').eq(idx).addClass('on');
+    });
+});
